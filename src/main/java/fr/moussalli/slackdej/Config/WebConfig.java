0c83141bfe8d777fea459bad1832926c38373612
mboup <mboup124@gmail.com>
2024-03-08 12:16:30 +0100

    edit WebConfig

--- src/main/java/fr/moussalli/slackdej/Config/WebConfig.java
+++ src/main/java/fr/moussalli/slackdej/Config/WebConfig.java
@@ -1,4 +1,4 @@
-package fr.moussalli.slackdej.CorsConfig;
+package fr.moussalli.slackdej.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
